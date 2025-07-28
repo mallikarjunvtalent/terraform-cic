0b087ad8b9c25f9971ef0bf458e8919d9acf03ee
mallikarjunvtalent <58244634+mallikarjunvtalent@users.noreply.github.com>
2025-07-28 18:32:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-05ffe3c48a9991133"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "test"
+    }
+}
